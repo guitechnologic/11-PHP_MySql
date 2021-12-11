79825065a01449b8a5f78a5743ed3e17b236e6f7
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2021-12-11 13:03:04 -0300

    Realizar upload de arquivo

--- public/09-RelizarUploadDeArquivos/inicial.php
+++ public/09-RelizarUploadDeArquivos/inicial.php
@@ -4,21 +4,20 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Curso PHP Integração com MySQL</title>
+        <title>Curso PHP INTEGRACAO</title>
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
     </head>
 
     <body>
-        <?php include_once("../_incluir/topo.php"); ?>
-        <?php include_once("../_incluir/funcoes.php"); ?>  
+        <?php include_once("_incluir/topo.php"); ?>
         
         <main>  
             
         </main>
 
-        <?php include_once("../_incluir/rodape.php"); ?>  
+        <?php include_once("_incluir/rodape.php"); ?>  
     </body>
 </html>
 
